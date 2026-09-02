f99563cb7f49f0feffda904a1372b4ff0295cae1
Ben <beboyce123@gmail.com>
2026-09-02 11:21:18 -0400

    Updated main (deploy)

--- movie-web-app/src/App.jsx
+++ movie-web-app/src/App.jsx
@@ -43,7 +43,7 @@ const App = () => {
             }
 
             const data = await response.json();
-
+            console.log(data);
             if (data.Response === 'False') {
                 setErrorMessage(data.Error || "Failed to fetch movies");
                 setMovieList([]);
